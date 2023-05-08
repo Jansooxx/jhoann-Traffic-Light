fb4e6ba3f80d95564265fd3884c48caffda8be06
Jhoann <64670851+Jansooxx@users.noreply.github.com>
2023-05-08 00:18:27 +0000

    Ajustes finales semaforo

--- src/js/component/Traffic.jsx
+++ src/js/component/Traffic.jsx
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 
 const Traffic = () => {
 
-    const [select, setSelect] = useState("")
-    const [green, setGreen] = useState("cirGreen")
-    const [yellow,setYellow] = useState("cirYell")
-    const [red, setRed] = useState("cirRed")
-    const [light, setLight] = useState("lighted")
+    const [select, setSelect]   = useState("")
+    const [green, setGreen]     = useState("cirGreen")
+    const [yellow,setYellow]    = useState("cirYell")
+    const [red, setRed]         = useState("cirRed")
+    const [light, setLight]     = useState("lighted")
 
     return (
         <>     
+            <div id="barra"></div>
             <div className="chassis">
-                <div className={select === red? light:red} onClick={()=>{select==red ? setSelect("") :setSelect(red)}}></div>
-                <div className={select === yellow ? light : yellow} onClick={()=>{select==yellow?setSelect(""):setSelect(yellow)}}></div>
-                <div className={select === green ? light : green} onClick={() => {select == green ? setSelect(""):setSelect(green)}}></div>
+                <div className={select === red? "cirRed lighted":red} onClick={()=>{select==red ? setSelect("") :setSelect(red)}}></div>
+                <div className={select === yellow ? "cirYell lighted " : yellow} onClick={()=>{select==yellow?setSelect(""):setSelect(yellow)}}></div>
+                <div className={select === green ? "cirGreen lighted" : green} onClick={() => {select== green ? setSelect(""):setSelect(green)}}></div>
             </div>
         </>
     )
